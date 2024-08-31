0d935c492aacec69bb12fe7d13526b9e886839ff
root <root@LAPTOP-2M9POCNM>
2024-08-31 17:19:09 +0530

    changes made in Total.jsx

--- frontend/src/components/Total1.jsx
+++ frontend/src/components/Total1.jsx
@@ -4,10 +4,9 @@ import Navbar from './Navbar'
 import Home from './Home'
 import Main from './Main'
 import Background from './Background'
-import AnimationText from './AnimationText'
 import Footer from './Footer'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import About from './About'
+
 
 function Total1() {
   return (
@@ -17,7 +16,6 @@ function Total1() {
     <Main />
     <Home />
     <Background />
-    <AnimationText />
     <Footer />
   </>
 
